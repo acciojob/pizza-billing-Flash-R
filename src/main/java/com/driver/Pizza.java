38fd1170091357a91b183f54440d2277a9b85be6
Tumwesigye Ronnie <tumronald1@gmail.com>
2023-07-03 01:13:13 +0300

    Ordered my Pizza'

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,37 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private boolean extraCheese;
+    private boolean extraToppings;
+    private boolean takeAway;
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Boolean getVeg() {
+        return isVeg;
+    }
+
+    public void setVeg(Boolean veg) {
+        isVeg = veg;
+    }
+
+    public void setBill(String bill) {
+        this.bill = bill;
+    }
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+//        a flag to track if cheese are needed
+        this.extraCheese = false;
+//        a flag to track if extra toppings are needed
+        this.extraToppings = false;
+        this.takeAway = false;
+//        set prices accordingly
+        if(this.isVeg){
+            this.price = 300;
+        }else this.price = 400;
     }
 
     public int getPrice(){
@@ -16,19 +43,49 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        // add extra cheese only if it is not already there
+        if(!this.extraCheese){
+            this.price += 80; //add the price for cheese
+            this.extraCheese = true; // flag on the cheese so that they are added again
+        }
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(!this.extraToppings){
+            if(this.isVeg)
+                this.price += 70;
+            else
+                this.price += 120;
+            this.extraToppings = true;
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        if(!this.takeAway){
+            this.price += 20; //add the price of paper bag
+            this.takeAway = true;
+        }
+
     }
 
     public String getBill(){
-        // your code goes here
+//        create a string builder to hold my bill
+        StringBuilder bill = new StringBuilder();
+        if(this.isVeg)
+            bill.append("Base Price Of The Pizza: 300 \n");
+        else
+            bill.append("Base Price Of The Pizza: 400\n");
+        if(this.extraCheese)
+            bill.append("Extra Cheese Added: 80\n");
+        if(this.extraCheese && this.isVeg)
+            bill.append("Extra Topping Added: 70\n");
+        if(this.extraCheese && !this.isVeg)
+            bill.append("Extra Topping Added: 120\n");
+        if(takeAway)
+            bill.append("Paperbag Added: 20\n");
+
+        bill.append("Total Price: " + this.price +"\n");
+        this.bill = bill.toString();
         return this.bill;
     }
 }
